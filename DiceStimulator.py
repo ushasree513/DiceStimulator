1daa80ee5c5b0783a1b3f2673c4c22c796f3e197
ushasree513 <ushasree513@gmail.com>
2022-08-14 12:50:33 +0530

    Updated Code

---
+++ DiceStimulator.py
@@ -0,0 +1,24 @@
+import random
+from tkinter import *
+
+root = Tk()
+root.geometry("800x500")
+##my_label=Label(root, text="Hello World, I am using tkinter")
+##my_label.pack()
+
+
+def roll():
+
+    value = True
+
+    while(value):
+        Result = random.randint(1,6)
+        print(Result)
+        ##CollectInfo = input("Do you want to play again? (n/y)")
+        
+        if(Result == 6):
+            continue
+        else:
+            break   
+b1 = Button(root, text="Lets roll...", command=roll).pack()
+root.mainloop()
